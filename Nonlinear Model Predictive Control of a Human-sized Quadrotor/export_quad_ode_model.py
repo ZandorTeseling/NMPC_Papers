Simulation is working. Need to check quaternion norm and do correct initialization of quaternions based on rpy rotation matrix.

Nonlinear Model Predictive Control of a Human-sized Quadrotor/export_quad_ode_model.py
@@ -91,19 +91,21 @@ def export_quad_ode_model():
     S[:,0] = vertcat(-q1,-q2,-q3)
     S[:,1:4] = q0*np.eye(3) - skew(vertcat(q1,q2,q3))
 
+    #Define angular velocity vector
     OMG = vertcat(omegax,omegay,omegaz)
 
-    print(cross(OMG, mtimes(J,OMG)).shape)
-
-
-
-
     p = vertcat(rho, A, Cl, Cd, m, g, J1, J2, J3)
-    print(p.shape)
-    # dynamics     
+
+    #Define torque applied to the system
+    T = SX(3,1)
+    T[0, 0] = 0.5 * A * Cl * rho *( w2*w2 - w4*w4)
+    T[1, 0] = 0.5 * A * Cl * rho * (w1 * w1 - w3 * w3)
+    T[2, 0] = 0.5 * A * Cl * rho * (w1 * w1 - w2 * w2 + w3*w3 - w4*w4)
+
+    # dynamics
     f_expl = vertcat(
-        #TODO map angular velocity to quaternion dynamics. Use the approximation?
-        #TODO normal dynamics for domega
+                        0.5*mtimes(transpose(S),OMG),
+                        mtimes(inv(J),( T - cross(OMG,mtimes(J,OMG))))
                      )
 
     f_impl = xdot - f_expl

Nonlinear Model Predictive Control of a Human-sized Quadrotor/minimal_example_sim.py
@@ -32,26 +32,27 @@
 #
 
 from acados_template import AcadosSim, AcadosSimSolver
-from export_pendulum_ode_model import export_pendulum_ode_model
-from utils import plot_pendulum
+from export_quad_ode_model import export_quad_ode_model
+from utils import plot_quad
 import numpy as np
 import matplotlib.pyplot as plt
 
 sim = AcadosSim()
 
 # export model 
-model = export_pendulum_ode_model()
+model = export_quad_ode_model()
 
 # set model_name 
 sim.model = model
 
-Tf = 0.1
+Tf = 1
+dt = 0.01
 nx = model.x.size()[0]
 nu = model.u.size()[0]
-N = 200
+
 
 # set simulation time
-sim.solver_options.T = Tf
+sim.solver_options.T = dt
 # set options
 sim.solver_options.num_stages = 4
 sim.solver_options.num_steps = 3
@@ -61,23 +62,48 @@ sim.solver_options.newton_iter = 3 # for implicit integrator
 # create
 acados_integrator = AcadosSimSolver(sim)
 
-simX = np.ndarray((N+1, nx))
-x0 = np.array([0.0, np.pi+1, 0.0, 0.0])
-u0 = np.array([0.0])
-acados_integrator.set("u", u0)
+
+Nsim = int(Tf/dt)
+uMax = 40 #rad/s control input max
+simX = np.ndarray((Nsim+1, nx))
+simU = np.ndarray((Nsim, nu))
+
+x0 = np.zeros((nx))
+x0[0] = 1
+#TODO Set up correct initialisation for quaternions based of a rpy inital condition!!
+u0 = np.zeros((nu))
 
 simX[0,:] = x0
 
-for i in range(N):
+#Parameters
+rho =   1.225
+A =     0.1
+Cl =    0.125
+Cd =    0.075
+m =     10.0
+g =     9.81
+J3 =    0.25
+J2 =    J3*4
+J1 =    J3*4
+p = np.array([rho, A, Cl, Cd, m, g, J1, J2, J3])
+acados_integrator.set("p", p)
+
+for i in range(Nsim):
     # set initial state
-    acados_integrator.set("x", simX[i,:])
+    acados_integrator.set("x", x0)
+    # set control inputs
+    u0 = 0.25 * uMax * np.random.randn(4)
+    acados_integrator.set("u", u0)
     # solve
     status = acados_integrator.solve()
     # get solution
-    simX[i+1,:] = acados_integrator.get("x")
+    x0 = acados_integrator.get("x")
 
-if status != 0:
-    raise Exception('acados returned status {}. Exiting.'.format(status))
+    simU[i,:] = u0
+    simX[i+1,:] = x0
+
+    if status != 0:
+        raise Exception('acados returned status {}. Exiting.'.format(status))
 
 # plot results
-plot_pendulum(Tf/N, 10, np.zeros((N, nu)), simX)
+plot_quad(dt, uMax, simU, simX)

Nonlinear Model Predictive Control of a Human-sized Quadrotor/utils.py
@@ -71,22 +71,34 @@ def plot_quad(h, u_max, U, X_true, X_est=None, Y_measured=None, latexify=True):
     Tf = N_sim*h
     t = np.linspace(0.0, Tf, N_sim)
 
+    control_labels = ['$w_1$',
+                      '$w_2$',
+                      '$w_3$',
+                      '$w_4$']
+
     if WITH_ESTIMATION:
         N_mhe = N_sim - X_est.shape[0]
         t_mhe = np.linspace(N_mhe, Tf, N_sim)
-
-    plt.subplot(nx+1, 1, 1)
-    plt.step(t[:U.shape[0]], U, color='r')
-    plt.title('closed-loop simulation')
+    plt.figure(1)
+    plt.subplot(1, 1, 1)
+    plt.step(t[:U.shape[0]], U,'o-')
+    plt.title('closed-loop control inputs')
     plt.ylabel('$u$')
     plt.xlabel('$t$')
     plt.hlines(u_max, t[0], t[-2], linestyles='dashed', alpha=0.7)
     plt.hlines(-u_max, t[0], t[-2], linestyles='dashed', alpha=0.7 )
     plt.ylim([1.2*u_max, -1.2*u_max])
     plt.grid()
+    plt.legend(control_labels)
 
-    states_lables = ['$x$', r'$\theta$', '$v$', r'$\dot{\theta}$']
-
+    states_lables = ['$q_0$',
+                     '$q_1$',
+                     '$q_2$',
+                     '$q_3$',
+                     '$\dot{\omega_1}$',
+                     '$\dot{\omega_2}$',
+                     '$\dot{\omega_3}$']
+    plt.figure(2)
     for i in range(nx):
         plt.subplot(nx+1, 1, i+2)
         plt.plot(t, X_true[:,i], label='true')